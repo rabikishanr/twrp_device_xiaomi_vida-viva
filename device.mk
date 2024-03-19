eeab18f884fbfbe50b336adf57a872b3784f34c5
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-03-19 18:14:57 +0545

    remove: conflicts

--- device.mk
+++ device.mk
@@ -17,7 +17,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
-	android.hardware.boot@1.2-service \
     android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-mtkimpl.recovery
 	
