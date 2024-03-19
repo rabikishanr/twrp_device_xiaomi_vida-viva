f5658c6d3aa9bf1f0dc12348fa911e36854a58d5
redeem-freefire <rabikishanr@gmail.com>
2024-03-19 17:29:34 +0545

    Viva: import blobs from V14.0.1.0.TGDEUXM

---
+++ twrp_viva.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from viva device
+$(call inherit-product, device/xiaomi/viva/device.mk)
+
+PRODUCT_DEVICE := viva
+PRODUCT_NAME := twrp_viva
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := viva
+PRODUCT_MANUFACTURER := xiaomi
