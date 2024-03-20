3692805bec2c67baa0c7cbfdfc581f00f983e623
Rabikishan Rauniyar <154669639+rabikishanr@users.noreply.github.com>
2024-03-20 19:06:44 +0545

    Viva: add some missing code

--- device.mk
+++ device.mk
@@ -15,10 +15,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-mtkimpl \
-    android.hardware.boot@1.0-mtkimpl.recovery
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
 	
 	
 PRODUCT_PACKAGES_DEBUG += \
